44c12bc9c00a573a8b721d40855486637507e1de
jysusdev <jesu.munoz@duocuc.cl>
2025-06-03 23:30:54 -0400

    3.1.3 completo

--- Ejercicios_clases/3.1.3.py
+++ Ejercicios_clases/3.1.3.py
@@ -64,4 +64,57 @@ while True:
         break       
     else:
         print("Ingrese 1, 2, 3 o 4")
-        
+
+# Ejercicio 2
+lista_super = {
+    "Producto" : [],
+    "Valor" : []
+}
+while True:
+    try:
+        print("----Bienvenido al super----")
+        print("(1). Agregar producto")
+        print("(2). Detalle boleta")
+        print("(3). Salir")
+        opc = int(input("Ingrese opción: "))
+    except ValueError:
+        print("Ingrese 1, 2 o 3")
+        continue
+
+    if opc == 1:
+        try:
+            producto = input("Ingrese producto: ")
+            precio = int(input("Ingrese el precio del producto: "))
+
+            if precio < 1:
+                raise Limite
+            
+            lista_super["Producto"].append(producto)
+            lista_super["Valor"].append(precio)
+        except Limite:
+            print("El valor del producto debe ser mayor a 0")
+            continue
+        except ValueError:
+            print("Ingrese un número")
+            continue
+        except:
+            print("A ocurrido un error")
+            continue
+    elif opc == 2:
+        try:
+            if not lista_super["Producto"]:
+                raise Vacio
+            
+            print("-----Detalle Boleta-----")
+            for i in range(len(lista_super["Producto"])):
+                elemento = lista_super["Producto"][i]
+                valor = lista_super["Valor"][i]
+                print(f"{elemento}:    ${valor}")
+            print(f"El valor total es: {sum(lista_super['Valor'])}")
+        except Vacio:
+            print("Debe agregar algún producto...")
+            continue
+    elif opc == 3:
+        print("Muchas gracias por su compra!")
+    else:
+        print("Ingrese 1, 2, o 3")
