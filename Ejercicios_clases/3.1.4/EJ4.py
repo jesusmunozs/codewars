d45444aeace5a2a3a05c17e870ec587fbe060727
jysusdev <jesu.munoz@duocuc.cl>
2025-06-10 14:00:38 -0400

    Todo

--- Ejercicios_clases/3.1.4/EJ4.py
+++ Ejercicios_clases/3.1.4/EJ4.py
@@ -7,5 +7,27 @@ precios = []
 tiempo = []
 media_movil = []
 
-for i in range(51):
-    precios.append(rm.randint)
+for t in range(51):
+    precios.append(rm.randint(1,20))
+    tiempo.append(t)
+
+periodos = 3
+for conteo in range(len(precios)-periodos+1):
+    promedio = 0
+    for item in range(periodos):
+        promedio += precios[conteo]/periodos
+    promedio = round(promedio/periodos)
+    media_movil.append(promedio)
+
+
+for i in range(periodos -1):
+    primero = media_movil[0]
+    media_movil.insert(0,primero)
+
+ax.plot(tiempo, precios)
+ax.plot(tiempo, media_movil)
+ax.set_title("Media Movil: Precio vs Tiempo")
+ax.set_xlabel("Tiempo")
+ax.set_ylabel("Precio")
+
+plt.show()
